65e7306496c19460380dfe7f36c2c0ecea766085
EARLMATTHEWS <EarlMatthews@users.noreply.github.com>
2025-03-10 11:40:04 -0400

    Refactor code structure by moving Point, Line, and Cell classes to separate files

--- main.py
+++ main.py
@@ -1,40 +1,8 @@
 from tkinter import Tk, Canvas
 import random
-
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-class Line:
-    def __init__(self, start, end):
-        self.start = start
-        self.end = end
-
-    def draw(self, canvas, fill_color):
-        canvas.create_line(self.start.x, self.start.y, self.end.x, self.end.y,fill=fill_color,width=2)
-
-class Cell:
-    def __init__(self, x1, y1, x2, y2,win):
-        self._x1 = x1
-        self._x2 = x2
-        self._y1 = y1
-        self._y2 = y2
-        self._win = win
-        self.has_left_wall = True
-        self.has_right_wall = True
-        self.has_top_wall = True
-        self.has_bottom_wall = True
-    
-    def draw(self):
-        if self.has_left_wall:
-            self._win.draw_line(Line(Point(self._x1,self._y1),Point(self._x1,self._y2)),'black')
-        if self.has_right_wall:
-            self._win.draw_line(Line(Point(self._x2,self._y1),Point(self._x2,self._y2)),'black')
-        if self.has_top_wall:
-            self._win.draw_line(Line(Point(self._x1,self._y1),Point(self._x2,self._y1)),'black')
-        if self.has_bottom_wall:
-            self._win.draw_line(Line(Point(self._x1,self._y2),Point(self._x2,self._y2)),'black')
+from point import Point
+from line import Line
+from cell import Cell
 
 class Window:
     def __init__(self,title="My Application", width=400, height=500):
