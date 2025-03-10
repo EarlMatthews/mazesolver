87fffc38b299a4cf450271ef2b81ee985a9ad8a9
EARLMATTHEWS <EarlMatthews@users.noreply.github.com>
2025-03-10 09:08:18 -0400

    Add Cell class and functionality to generate and draw random cells

--- main.py
+++ main.py
@@ -14,6 +14,28 @@ class Line:
     def draw(self, canvas, fill_color):
         canvas.create_line(self.start.x, self.start.y, self.end.x, self.end.y,fill=fill_color,width=2)
 
+class Cell:
+    def __init__(self, x1, y1, x2, y2,win):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        self._win = win
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+    
+    def draw(self):
+        if self.has_left_wall:
+            self._win.draw_line(Line(Point(self._x1,self._y1),Point(self._x1,self._y2)),'black')
+        if self.has_right_wall:
+            self._win.draw_line(Line(Point(self._x2,self._y1),Point(self._x2,self._y2)),'black')
+        if self.has_top_wall:
+            self._win.draw_line(Line(Point(self._x1,self._y1),Point(self._x2,self._y1)),'black')
+        if self.has_bottom_wall:
+            self._win.draw_line(Line(Point(self._x1,self._y2),Point(self._x2,self._y2)),'black')
+
 class Window:
     def __init__(self,title="My Application", width=400, height=500):
         self.width = width
@@ -49,13 +71,22 @@ def generate_random_lines(num_lines):
         lines.append(Line(start, end))
     return lines    
 
+def generate_random_cells(num_cells,win):
+    cells = []
+    for i in range(num_cells):
+        x1 = random.randint(0, 800)
+        x2 = random.randint(0, 800)
+        y1 = random.randint(0, 600)
+        y2 = random.randint(0, 600)
+        cells.append(Cell(x1,y1,x2,y2,win))
+    return cells
 
 def main():
     win = Window("My Application",800,600)
      # Create points and a line
-    for line in generate_random_lines(1000):
-        win.draw_line(line,'red')
-
+    for cell in generate_random_cells(10,win):
+        cell.draw()
+    
     win.wait_for_close()
     
 if __name__ == "__main__":
